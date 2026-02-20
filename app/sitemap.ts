dc7460c5540f69c5986c83f5250a01a52a6757bf
uthaipan <uthaipan.aon@gmail.com>
2026-02-20 22:41:49 +0700

    update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,11 +2,11 @@ import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
     // Replace with your actual production URL
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || ""
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
 
     return [
         {
-            url: `${baseUrl}`,
+            url: `${baseUrl}/`,
             lastModified: new Date(),
             changeFrequency: 'daily',
             priority: 1,
